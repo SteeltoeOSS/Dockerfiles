188c121f1704e316c7ba8c490e84daa656225b2c
Chris Cheetham <chris@cheetham.com>
2018-05-22 17:20:36 -0400

    Add support for Spring Cloud Eureka Server

---
+++ eurekaserver/src/main/java/io/steeltoe/samples/EurekaServer.java
@@ -0,0 +1,17 @@
+package io.steeltoe.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+public class EurekaServer {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+
+}
