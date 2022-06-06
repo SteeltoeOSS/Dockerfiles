49a261dc98c0e190c3dd859bb74acc70fbf099b3
Chris Cheetham <chris@cheetham.com>
2022-06-06 11:42:23 -0400

    Log version on startup

--- eureka-server/files/src/main/java/io/steeltoe/docker/eurekaserver/EurekaServer.java
+++ eureka-server/files/src/main/java/io/steeltoe/docker/eurekaserver/EurekaServer.java
@@ -1,5 +1,7 @@
 package io.steeltoe.docker.eurekaserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,8 +10,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServer.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(EurekaServer.class);
+
+    public static void main(String[] args) {
+        Package pkg = EnableEurekaServer.class.getPackage();
+        logger.info("{} {} by {}", pkg.getImplementationTitle(), pkg.getImplementationVersion(), pkg.getImplementationVendor());
+        SpringApplication.run(EurekaServer.class, args);
+    }
 
 }
